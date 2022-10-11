bc5256e3221d23ebe3060e9e8a190ac1c1ec0597
leonbos1 <ktmleon@live.nl>
2022-10-11 16:31:01 +0200

    Eerste versie

--- Controllers/TwitterController.cs
+++ Controllers/TwitterController.cs
@@ -1,30 +1,60 @@
-﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+﻿using BencomTwitterApp.Models;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace BencomTwitterApp.Controllers
 {
     public class TwitterController : Controller
     {
- 
+
+        public static string TwitterUsername = "geertwilderspvv";
+
         public IActionResult Index()
         {
-
+         
             var bearer_token = "AAAAAAAAAAAAAAAAAAAAAEswiAEAAAAAN5n12SejPIh8osChcGjzXK%2BXiqw%3DBrgpvcLXgQ6iROiZ139Cq4QVRLjDTZXlq6f9NKlPBhjF3HjBOj";
+            Debug.WriteLine(TwitterUsername);
 
-            using (var client = new HttpClient())
+            var url = string.Format("https://api.twitter.com/2/tweets/search/recent?query=from:{0}", TwitterUsername);
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearer_token);
+
+            try
             {
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearer_token);
-                var endpoint = new Uri("https://api.twitter.com/2/tweets?ids=1228393702244134912");
-                var result = client.GetAsync(endpoint).Result;
-                var json = result.Content.ReadAsStringAsync().Result;
-                //Debug.WriteLine(result);
-                Debug.WriteLine(json);
+                var response = client.GetAsync(url).Result;
+                string jsonResponse = response.Content.ReadAsStringAsync().Result;
+                Debug.WriteLine(jsonResponse);
+
+                var tweets = JsonConvert.DeserializeObject<Root>(jsonResponse);
+
+                //Debug.WriteLine(tweets.data[0].text);
+
+                return View(tweets);
 
             }
-      
+
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+
+            }
+
             return View();
+
+        }
+
+        public IActionResult setUser(string Username)
+        {
+            if (TwitterUsername != "")
+            {
+                TwitterUsername = Username;
+            }
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- Models/Tweet.cs
+++ Models/Tweet.cs
@@ -1,6 +1,19 @@
-﻿namespace BencomTwitterApp.Models
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace BencomTwitterApp.Models
 {
     public class Tweet
     {
+        public List<string> edit_history_tweet_ids { get; set; }
+        public string id { get; set; }
+        public string text { get; set; }
     }
+
+    public class Root
+    {
+        public string Username { get; set; }
+        public List<Tweet> data { get; set; }
+    }
+
 }
